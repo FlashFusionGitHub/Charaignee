af5ce45e6789cae149230f0ec8be9e559dfa9e2d
AIE\s162464 <ewhhday@gmail.com>
2017-11-07 09:27:05 +1030

    Added partial lighting

---
+++ Assets/Scripts/Flickeringlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickeringlight : MonoBehaviour {
+
+    Light testLight;
+    public float minWaitTime;
+    public float maxW;
+
+	void Start() { 
+     
+        testLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+      
+}
+
+    IEnumerator Flashing ()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            testLight.enabled = ! testLight.enabled;
+        }	
+	}
+}
